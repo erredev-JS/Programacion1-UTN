cdbdd67368f6c6e17cd304deda351019fdd16c61
erredev-JS <roddev100@gmail.com>
2023-08-17 17:45:09 -0300

    Arreglando diferencia entre repositorio local y remotoç

---
+++ ejercicios-complementarios01.py
@@ -0,0 +1,75 @@
+# 1
+
+numero1 = 25
+
+# 2
+
+numero2 = 2.00
+
+# 3
+
+sum = numero1 + numero2
+
+# 4
+
+res = numero1 - numero2
+div = numero1 / numero2
+mult = numero1 * numero2
+print(f'Resta: {res}, Division: {div}, Multiplicacion: {mult}')
+
+# 5 
+
+nombre = 'Rodrigo'
+
+# 6
+
+precio = 1300.25
+
+# 7
+
+descuento = 0.5
+
+# 8
+
+precio_final = precio - (precio * descuento)
+
+# 9 
+
+cadena = 'Esto es una cadena'
+
+# 10
+
+longitud = len(cadena)
+
+# 11
+
+precio = 120.50
+precio = int(precio)
+
+# 12
+
+nombre = 'Rodrigo'
+apellido = 'Zapata'
+nombre_completo = nombre + ' ' + apellido
+
+# 13
+
+edad = 19
+edad += 5
+edad -= 10
+
+# 14
+
+altura = 1.75
+altura *= 4
+altura /= 3
+
+# 15
+
+nombre_mayus = 'RODRIGO'
+nombre_mayus = nombre_mayus.lower()
+
+# 16
+nombre_mayus = 'RODRIGO'
+
+nombre_mayus = nombre_mayus.capitalize()
