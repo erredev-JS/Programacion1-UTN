5409f7859819a504ffbe3a5b8df00f2010893761
Rodrigo Zapata <118647241+erredev-JS@users.noreply.github.com>
2023-08-17 08:19:27 -0300

    Corrección ejercicios-complementarios01.py
    Faltaba imprimir por pantalla los resultados.

--- ejercicios-complementarios01.py
+++ ejercicios-complementarios01.py
@@ -1,14 +1,17 @@
 # 1
 
 numero1 = 25
+print(numero1)
 
 # 2
 
 numero2 = 2.00
+print(numero2)
 
 # 3
 
 sum = numero1 + numero2
+print(sum)
 
 # 4
 
@@ -20,56 +23,68 @@ print(f'Resta: {res}, Division: {div}, Multiplicacion: {mult}')
 # 5 
 
 nombre = 'Rodrigo'
+print(nombre)
 
 # 6
 
 precio = 1300.25
+print(precio)
 
 # 7
 
 descuento = 0.5
+print(descuento)
 
 # 8
 
 precio_final = precio - (precio * descuento)
+print(precio_final)
 
 # 9 
 
 cadena = 'Esto es una cadena'
+print(cadena)
 
 # 10
 
 longitud = len(cadena)
+print(longitud)
 
 # 11
 
 precio = 120.50
 precio = int(precio)
+print(precio)
 
 # 12
 
 nombre = 'Rodrigo'
 apellido = 'Zapata'
 nombre_completo = nombre + ' ' + apellido
+print(nombre_completo)
 
 # 13
 
 edad = 19
 edad += 5
 edad -= 10
+print(edad)
 
 # 14
 
 altura = 1.75
 altura *= 4
 altura /= 3
+print(altura)
 
 # 15
 
 nombre_mayus = 'RODRIGO'
 nombre_mayus = nombre_mayus.lower()
+print(nombre_mayus)
 
 # 16
-nombre_mayus = 'RODRIGO'
 
+nombre_mayus = 'RODRIGO'
 nombre_mayus = nombre_mayus.capitalize()
+print(nombre_mayus)
