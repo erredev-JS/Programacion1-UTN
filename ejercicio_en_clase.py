5b2565f821693e0d949c99c54001914e45bf4fc0
erredev-JS <roddev100@gmail.com>
2023-08-23 16:31:55 -0300

    Finalizacion ejercicios_en_clase

--- ejercicio_en_clase.py
+++ ejercicio_en_clase.py
@@ -1,18 +1,35 @@
+dias = ('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes')
 
-# Retocar, no funciona
+print('Ingresa datos solicitados:\n')
 
-print('Ingresa datos solicitados: \n')
 fecha = input('Formato día, /dd/mm: ')
-dia_sem = fecha[0:fecha.find(',')]
-dia_sem = dia_sem.lower()
-print()
 
-dia = int(fecha[fecha.find(' ')+1:fecha.find('/')])
+dia_sem = fecha[0:fecha.find(',')].capitalize()
 
-mes = int(fecha[fecha.find('/') + 1:])
-print(dia_sem)
+dia = int(fecha[fecha.find('/') + 1:fecha.find('/', fecha.find('/') + 1)])
+mes = int(fecha[fecha.rfind('/') + 1:])
 
-if((dia_sem == 'lunes' or dia_sem == 'martes' or dia_sem == 'miercoles' or dia_sem == 'jueves' or dia_sem == 'viernes')and ((dia < 31 and dia < 0) and (mes > 0 and mes < 12))):
-  print(dia_sem,dia,mes )
+if (dia_sem in dias) and ((dia > 0 and dia <= 31) and (mes > 0 and mes <= 12)):
+  if(dia_sem == 'Lunes' or dia_sem == 'Martes' or dia_sem == 'Miercoles'):
+      examenes = input('Hoy se tomaron los examenes?: \n (Responder "Si o No")\n')
+      if(examenes == 'Si'):
+         aprobaron =  int(input('Cuantos estudiantes aprobaron?:\n'))      
+         desaprobaron = int(input('Cuantos estudiantes no aprobaron?:\n'))
+         total_alumnos = aprobaron + desaprobaron
+         porcentaje_aprobados = (aprobaron / total_alumnos) * 100
+         print(f'El porcentaje de alumnos aprobados es {porcentaje_aprobados}%')
+  elif(dia_sem == 'Jueves'):
+      porcentaje_asistencia = float(input('Ingresa el porcentaje de asistencia a clase sin el "%":\n'))  
+      if(porcentaje_asistencia > 50):
+          print('Asistio la mayoria')
+      else:
+          print('No asistio la mayoria')     
+  elif(dia_sem == 'Viernes'):
+      if(dia == 1 and(mes == 1 or mes == 7)):
+          print('Comienzo de nuevo ciclo\n')
+          cantidad_alumnos = int(input('Ingresar cantidad de alumnos del nuevo ciclo:\n'))       
+          monto_arancel = float(input('Ingresar cantidad de alumnos del nuevo ciclo:\n'))
+          ingreso_total = monto_arancel * cantidad_alumnos
+          print(f'El ingreso total es de ${ingreso_total}')       
 else:
-  print('Error de entrada')
+      print('Error de entrada')
