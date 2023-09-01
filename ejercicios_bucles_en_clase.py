7b4b973b67f92b53629582b27662430b543df69b
Rodrigo Zapata <118647241+erredev-JS@users.noreply.github.com>
2023-08-31 23:41:44 -0300

    Update ejercicios_bucles_en_clase.py

--- ejercicios_bucles_en_clase.py
+++ ejercicios_bucles_en_clase.py
@@ -20,15 +20,20 @@ else:
       print(f' El mensaje encriptado es: {msj_encriptado}')
 
 # Ejercicio 2
-n = 1
-par = 0
-inpar = 0
-while n != 0:
-  n = int(input('Ingresa un numero entero positivo: '))
-  if(n < 0):
-      print('Recuerda ingresar un numero entero positivo!')
-  elif(n % 2 == 0 and n != 0):
-      par += 1
-  elif(n % 1 == 0 and n != 0):
-      inpar += 1
-print(f'Ingresaste 0, los numeros pares ingresados fueron {par} y los numeros inpares fueron {inpar}')
+n = int(input('Ingresa un numero entero positivo: '))
+num_str = str(n)
+par_count = 0
+impar_count = 0
+
+for char in num_str:
+    digit = int(char)
+    resto = digit % 2
+    if resto == 0:
+        print(f'El dígito {digit} es par.')
+        par_count += 1
+    else:
+        print(f'El dígito {digit} es impar.')
+        impar_count += 1
+
+print(f'En el número {n}, hay {par_count} dígitos pares y {impar_count} dígitos impares.')
+
